e7c18ac976c8ea26c8e3dfbd1b76f255749e3801
orflores <orflores@rassini.com>
2021-01-22 11:33:30 -0600

    Examples for fromEvent and range

---
+++ src/observables/05-fromEvent.ts
@@ -0,0 +1,20 @@
+import { fromEvent } from "rxjs";
+
+/*
+* Form DOM Events
+*/
+
+const src1$ = fromEvent<MouseEvent>( document, "click" );
+const src2$ = fromEvent<KeyboardEvent>( document, "keyup" );
+
+const observer = {
+	next: ( val: any ) => console.log("[NEXT]", val)
+}
+
+src1$.subscribe( ({ x, y }) => {
+	console.log( x, y )
+} );
+
+src2$.subscribe( ( evnt ) => {
+	console.log(evnt)
+} );
