f7f6614bbf6a7b9ef31257e7ebd75d6145420494
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2021-06-17 15:21:28 -0500

    add operator map pluk mapTo filter and tap

---
+++ src/operators/01-map-pluck-mapTo.ts
@@ -0,0 +1,34 @@
+import { fromEvent, range } from 'rxjs';
+import { map, mapTo, pluck } from 'rxjs/operators';
+ 
+// range(1,5).pipe(
+//     map<number, string>( val => (val * 10).toString() )
+// )
+// .subscribe(console.log);
+
+const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup');
+keyup$.subscribe( console.log );
+
+/**
+ * map must return somthing
+ */
+const keyupMap$ = keyup$.pipe(
+    map( event => event.code )
+)
+keyupMap$.subscribe( code => console.log('map', code) ) ;
+
+/**
+ * pluck return just information if you already know the name
+ */
+const keyupPluck$ = keyup$.pipe(
+    pluck('target', 'baseURI')
+);
+keyupPluck$.subscribe( code => console.log('pluck', code) ) ;
+
+/**
+ * mapTo 
+ */
+const keyupMapTo$ = keyup$.pipe(
+    mapTo('tecla presionada')
+);
+keyupMapTo$.subscribe( code => console.log('mapTo', code) ) ;
---
+++ src/operators/02-filter.ts
@@ -0,0 +1,38 @@
+import { from, fromEvent, range } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+range(20, 30).pipe(
+    filter((val, i) => {
+        console.log('index', i);
+        return val % 2 === 1;
+    })
+)//.subscribe( console.log );
+
+interface PersonajeInterface {
+    tipo: string;
+    nombre: string;
+}
+const personajes: PersonajeInterface[] = [
+    { tipo: 'heroe', nombre: 'Batman' },
+    { tipo: 'heroe', nombre: 'Robin' },
+    { tipo: 'villano', nombre: 'Joker' },
+]
+
+
+from(personajes).pipe(
+    filter((personaje: any) => {
+        return personaje.tipo === 'heroe'
+    })
+)// .subscribe( console.log );
+
+/**
+ * When condadenate pipes
+ * the order is important
+ */
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup').pipe(
+    map( event => event.code ),
+    filter( key => key === 'Enter' )
+);
+
+keyup$.subscribe(console.log);
